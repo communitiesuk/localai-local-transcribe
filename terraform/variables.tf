7ec579af40b278d5296d0f2bbe8c151ed3a7d3a1
Harry Best <harry.best@softwire.com>
2026-02-26 16:58:53 +0000

    chore: remove unneeded task_defintion_created variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,12 +4,6 @@ variable "ssl_certs_created" {
   default     = true
 }
 
-variable "task_definition_created" {
-  description = "Indicates whether the initial task definition has been created"
-  type        = bool
-  default     = true
-}
-
 variable "image_tag" {
   description = "The image tag to be used for all of frontend, backend, and worker"
   type        = string
