8885c7681e221de5f544202d11a9bbc110e8b5ec
Harry Best <harry.best@softwire.com>
2026-02-27 11:20:51 +0000

    chore: remove deprecated failure threshold parameter

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -92,8 +92,4 @@ resource "aws_service_discovery_service" "backend_service_discovery_service" {
 
     routing_policy = "MULTIVALUE"
   }
-
-  health_check_custom_config {
-    failure_threshold = 1
-  }
 }
