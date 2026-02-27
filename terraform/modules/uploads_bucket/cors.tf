5ce1d21836617565301b555881c451466d1e37a0
Harry Best <harry.best@softwire.com>
2026-02-27 11:31:02 +0000

    fix: remove deprecated expected_bucket_owner argument

--- terraform/modules/uploads_bucket/cors.tf
+++ terraform/modules/uploads_bucket/cors.tf
@@ -1,8 +1,6 @@
 resource "aws_s3_bucket_cors_configuration" "cors" {
   bucket = module.uploads_bucket.bucket_id
 
-  expected_bucket_owner = data.aws_caller_identity.current.account_id #deprecated field
-
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "GET", "POST"]
