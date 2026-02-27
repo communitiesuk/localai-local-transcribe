b03d093bb441eacdf16085b59b08c9b82b2b610b
Harry Best <harry.best@softwire.com>
2026-02-27 15:38:07 +0000

    feat: grant worker service access to uploads bucket

--- terraform/modules/uploads_bucket/iam.tf
+++ terraform/modules/uploads_bucket/iam.tf
@@ -27,3 +27,29 @@ data "aws_iam_policy_document" "uploads_bucket" {
     }
   }
 }
+
+data "aws_iam_policy_document" "ecs_bucket_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Put*",
+      "s3:Delete*",
+    ]
+    resources = [
+      "${module.uploads_bucket.bucket_arn}/app_data/*",
+      "${module.uploads_bucket.bucket_arn}/app_data",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecs_bucket_access" {
+  name   = "${var.environment_name}-ecs-bucket-access"
+  policy = data.aws_iam_policy_document.ecs_bucket_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_bucket_access" {
+  role       = var.worker_task_execution_role_name
+  policy_arn = aws_iam_policy.ecs_bucket_access.arn
+}
--- terraform/modules/uploads_bucket/variables.tf
+++ terraform/modules/uploads_bucket/variables.tf
@@ -31,3 +31,8 @@ variable "app_host" {
   description = "Application url"
   type        = string
 }
+
+variable "worker_task_execution_role_name" {
+  description = "Name of the IAM role for the worker ECS task execution"
+  type        = string
+}
