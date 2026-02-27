f7d25195cc73b8d32170f0d82e65def4db9afec2
Harry Best <harry.best@softwire.com>
2026-02-27 15:52:11 +0000

    feat: remove transcribe's access to the upload bucket

--- terraform/modules/uploads_bucket/iam.tf
+++ terraform/modules/uploads_bucket/iam.tf
@@ -1,33 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_iam_policy_document" "uploads_bucket" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-      "s3:GetBucketLocation",
-      "s3:DeleteObject"
-    ]
-    resources = [
-      module.uploads_bucket.bucket_arn,
-      "${module.uploads_bucket.bucket_arn}/*",
-    ]
-    principals {
-      type = "Service"
-      identifiers = [
-        "transcribe.amazonaws.com"
-      ]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values = [
-        data.aws_caller_identity.current.account_id,
-      ]
-    }
-  }
-}
-
 data "aws_iam_policy_document" "ecs_bucket_access" {
   statement {
     effect = "Allow"
