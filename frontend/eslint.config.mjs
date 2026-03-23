95fcf1bd9c9ce31aabe1e6cd46042b8d6e0242fa
Harry Best <harry.best@softwire.com>
2026-03-23 14:34:34 +0000

    chore: upgrade eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,22 +1,11 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['lib/client/**/*']),
-  {
-    extends: [
-      ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-    ],
-  },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettier,
 ])
