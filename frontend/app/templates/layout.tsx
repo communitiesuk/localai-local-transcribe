98e034b8b9f8621bfdf36916828452416d2907b8
Harry Best <150065627+harrybest-softwire@users.noreply.github.com>
2026-03-26 15:02:17 +0000

    chore: upgrade to nextjs 15 (#105)
    * chore: run nextjs15 upgrade codemod
    
    * chore: fix linting errors
    
    * fix: correct css syntax
    
    * fix: remove import of client side variable to fix proxy
    
    * chore: remove deprecated eslint config
    
    * fix: specify language in html root
    
    Co-authored-by: Warren Keith <w@rren.dev>
    
    ---------
    
    Co-authored-by: Warren Keith <w@rren.dev>

--- frontend/app/templates/layout.tsx
+++ frontend/app/templates/layout.tsx
@@ -1,10 +1,10 @@
 import { HistoryBackButton } from '@/components/ui/history-back-button'
-import { ReactElement } from 'react'
+import { ReactNode } from 'react'
 
 export default async function TemplatesLayout({
   children,
 }: {
-  children: ReactElement
+  children: ReactNode
 }) {
   return (
     <div className="p-6 pt-1">
