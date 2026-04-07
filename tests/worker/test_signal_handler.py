3e453dbd21a2d630bbd9a7eb0e82875fa3726fda
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2026-04-07 14:40:09 +0100

    test: split signal handler test

--- tests/worker/test_signal_handler.py
+++ tests/worker/test_signal_handler.py
@@ -1,3 +1,5 @@
+import signal
+
 from worker.signal_handler import SignalHandler
 
 
@@ -5,5 +7,8 @@ def test_signal_handler_initial_state():
     handler = SignalHandler()
     assert handler.signal_received is False
 
-    handler._handle_signal(15, None)  # noqa: SLF001
+
+def test_signal_handler_sets_flag_after_signal():
+    handler = SignalHandler()
+    handler._handle_signal(int(signal.SIGTERM), None)  # noqa: SLF001
     assert handler.signal_received is True
