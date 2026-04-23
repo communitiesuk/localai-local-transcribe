cd7b0779a54c3a377393734bf346260405b4992b
Harry Best <harry.best@softwire.com>
2026-04-23 16:38:33 +0100

    feat: enforce tls for sqs queues

--- terraform/modules/sqs/iam.tf
+++ terraform/modules/sqs/iam.tf
@@ -34,3 +34,35 @@ resource "aws_iam_role_policy_attachment" "worker_sqs_access" {
   role       = var.worker_task_role_name
   policy_arn = aws_iam_policy.sqs_access.arn
 }
+
+locals {
+  sqs_queues = {
+    transcription_queue            = aws_sqs_queue.transcription_queue
+    transcription_queue_deadletter = aws_sqs_queue.transcription_queue_deadletter
+    llm_queue                      = aws_sqs_queue.llm_queue
+    llm_queue_deadletter           = aws_sqs_queue.llm_queue_deadletter
+  }
+}
+
+resource "aws_sqs_queue_policy" "enforce_tls" {
+  for_each  = local.sqs_queues
+  queue_url = each.value.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyNonTLS"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "sqs:*"
+        Resource  = each.value.arn
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
