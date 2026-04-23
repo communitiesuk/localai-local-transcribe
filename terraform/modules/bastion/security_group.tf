5a093a41941ed76f8f0d3b48482a4caa03916f86
Harry Best <harry.best@softwire.com>
2026-04-23 14:58:35 +0100

    fix: remove bastion's access to frontend

--- terraform/modules/bastion/security_group.tf
+++ terraform/modules/bastion/security_group.tf
@@ -19,12 +19,4 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = [var.vpc_cidr_block]
     description = "Allow bastion access to postgres port within the VPC"
   }
-
-  egress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr_block]
-    description = "Allow bastion access to frontend port within the VPC"
-  }
 }
