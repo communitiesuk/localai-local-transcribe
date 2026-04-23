ba426663768a81b73af7c6b0f306ecb75ca2789e
Harry Best <harry.best@softwire.com>
2026-04-23 17:18:16 +0100

    chore: increase log retention to 1 year

--- terraform/modules/ecs/logging.tf
+++ terraform/modules/ecs/logging.tf
@@ -2,7 +2,7 @@
 #tfsec:ignore:aws-cloudwatch-log-group-customer-key
 resource "aws_cloudwatch_log_group" "frontend_log_group" {
   name              = "${var.environment_name}-frontend"
-  retention_in_days = 60
+  retention_in_days = 365
 
   tags = {
     Application = var.environment_name
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_log_group" "frontend_log_group" {
 
 resource "aws_cloudwatch_log_group" "backend_log_group" {
   name              = "${var.environment_name}-backend"
-  retention_in_days = 60
+  retention_in_days = 365
 
   tags = {
     Application = var.environment_name
@@ -20,7 +20,7 @@ resource "aws_cloudwatch_log_group" "backend_log_group" {
 
 resource "aws_cloudwatch_log_group" "worker_log_group" {
   name              = "${var.environment_name}-worker"
-  retention_in_days = 60
+  retention_in_days = 365
 
   tags = {
     Application = var.environment_name
