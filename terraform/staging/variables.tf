93a5187c9cdabdabf698e8eb8abc428340878ece
Harry Best <harry.best@softwire.com>
2026-04-30 15:26:24 +0100

    feat: add staging environment

---
+++ terraform/staging/variables.tf
@@ -0,0 +1,17 @@
+variable "ssl_certs_created" {
+  description = "Indicates whether ssl certificates have already been manually created"
+  type        = bool
+  default     = false
+}
+
+variable "image_tag" {
+  description = "The image tag to be used for all of frontend, backend, and worker"
+  type        = string
+  default     = "latest"
+}
+
+variable "alarm_email_address" {
+  description = "Email address to receive CloudWatch alarm notifications"
+  type        = string
+  sensitive   = true
+}
