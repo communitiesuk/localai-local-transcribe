245c467280a662742b14e455f318897774c03c4f
robbfox <robbfox@outlook.com>
2026-05-06 16:37:01 +0100

    chore: standardize quoting and formatting in migration merge file

--- alembic/versions/23ca09738767_merge_conflicting_database_migrations.py
+++ alembic/versions/23ca09738767_merge_conflicting_database_migrations.py
@@ -5,6 +5,7 @@ Revises: bf3e4dac2dcd, cbb794e0ad72
 Create Date: 2026-05-06 16:33:47.800386
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -13,8 +14,8 @@ import sqlmodel
 
 
 # revision identifiers, used by Alembic.
-revision: str = '23ca09738767'
-down_revision: Union[str, None] = ('bf3e4dac2dcd', 'cbb794e0ad72')
+revision: str = "23ca09738767"
+down_revision: Union[str, None] = ("bf3e4dac2dcd", "cbb794e0ad72")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
