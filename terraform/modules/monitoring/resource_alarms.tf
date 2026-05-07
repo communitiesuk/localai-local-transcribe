21542641497c238cf6f9f0f418b128d3475946fa
Toluwalope Lawal <toluwalope.lawal@communities.gov.uk>
2026-05-07 12:27:03 +0100

    feat(terraform): Create cloudwatch metrics (AIILG-297) (#178)
    * change to ecs_events, cleaning up other prs
    
    * from rds onwards
    
    * on hold for test-data
    
    * merged with dev
    
    * fix: align lockfile with dev
    
    * included detail type and a more descriptive name
    
    * post pr review
    
    ---------
    
    Co-authored-by: Toluwalope <toluwalopelawal1@DGTL0007.local>

--- terraform/modules/monitoring/resource_alarms.tf
+++ terraform/modules/monitoring/resource_alarms.tf
@@ -45,10 +45,12 @@ resource "aws_cloudwatch_metric_alarm" "ecs_memory_usage" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ecs_task_start_failure" {
-  alarm_name          = "ecs-task-start-failure"
+  for_each = aws_cloudwatch_log_metric_filter.ecs_task_start_failure
+
+  alarm_name          = "${each.key}-task-start-failure"
   alarm_description   = "An ECS task has failed to start and reach a healthy state"
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  metric_name         = aws_cloudwatch_log_metric_filter.ecs_task_start_failure.name
+  metric_name         = each.value.name
   namespace           = "LogMetrics"
   evaluation_periods  = 1
   period              = 60
@@ -76,6 +78,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_usage" {
     DBInstanceIdentifier = var.database_identifier
   }
 
+
   alarm_actions = [
     aws_sns_topic.alarm_sns_topic.arn,
   ]
@@ -95,7 +98,7 @@ resource "aws_cloudwatch_metric_alarm" "rds_storage" {
   dimensions = {
     DBInstanceIdentifier = var.database_identifier
   }
-
+  
   alarm_actions = [
     aws_sns_topic.alarm_sns_topic.arn,
   ]
