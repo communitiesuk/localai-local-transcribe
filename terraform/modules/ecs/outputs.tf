79245da5e84fa4e05ea602fed623818a16cba77e
Toluwalope Lawal <toluwalope.lawal@communities.gov.uk>
2026-05-13 17:41:03 +0100

    feat(terraform): Validate Terraform (AIILG-165) (#187)
    * added lock flag on plan step
    
    ---------
    
    Co-authored-by: Toluwalope <toluwalopelawal1@DGTL0007.local>

--- terraform/modules/ecs/outputs.tf
+++ terraform/modules/ecs/outputs.tf
@@ -9,42 +9,42 @@ output "ecs_cluster_name" {
 }
 
 output "backend_execution_task_name" {
-  value = aws_iam_role.ecs_task_execution.name
+  value       = aws_iam_role.ecs_task_execution.name
   description = "The name of the task execution role used by the backend ecs task"
 }
 
 output "worker_execution_task_name" {
-  value = aws_iam_role.ecs_task_execution.name
+  value       = aws_iam_role.ecs_task_execution.name
   description = "The name of the task execution role used by the worker ecs task"
 }
 
 output "frontend_execution_task_id" {
-  value = aws_iam_role.ecs_task_execution.id
+  value       = aws_iam_role.ecs_task_execution.id
   description = "The id of the task execution role used by the frontend ecs task"
 }
 
 output "backend_execution_task_id" {
-  value = aws_iam_role.ecs_task_execution.id
+  value       = aws_iam_role.ecs_task_execution.id
   description = "The id of the task execution role used by the backend ecs task"
 }
 
 output "worker_execution_task_id" {
-  value = aws_iam_role.ecs_task_execution.id
+  value       = aws_iam_role.ecs_task_execution.id
   description = "The id of the task execution role used by the worker ecs task"
 }
 
 output "frontend_execution_task_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
+  value       = aws_iam_role.ecs_task_execution.arn
   description = "The arn of the task execution role used by the frontend ecs task"
 }
 
 output "backend_execution_task_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
+  value       = aws_iam_role.ecs_task_execution.arn
   description = "The arn of the task execution role used by the backend ecs task"
 }
 
 output "worker_execution_task_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
+  value       = aws_iam_role.ecs_task_execution.arn
   description = "The arn of the task execution role used by the worker ecs task"
 }
 
@@ -60,15 +60,15 @@ output "worker_task_role_name" {
 
 output "frontend_service_name" {
   description = "The name of the frontend ecs service"
-  value = aws_ecs_service.frontend.name
+  value       = aws_ecs_service.frontend.name
 }
 
 output "backend_service_name" {
   description = "The name of the backend ecs service"
-  value = aws_ecs_service.backend.name
+  value       = aws_ecs_service.backend.name
 }
 
 output "worker_service_name" {
   description = "The name of the worker ecs service"
-  value = aws_ecs_service.worker.name
+  value       = aws_ecs_service.worker.name
 }
