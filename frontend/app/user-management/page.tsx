dd69882024ac626fff3b976dcdf17926eb2581a1
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2026-06-01 14:01:31 +0100

    move useUsers to hook

--- frontend/app/user-management/page.tsx
+++ frontend/app/user-management/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { ChevronLeft, Loader, Loader2 } from 'lucide-react'
+import { ChevronLeft } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { useUsers } from '@/hooks/use-users'
 
 export default function SupportPage() {
+  const organisation_id = 'placeholder'
+  const { users } = useUsers(organisation_id)
+
   const router = useRouter()
   return (
     <div className="mx-auto max-w-3xl pt-1">
@@ -24,6 +28,14 @@ export default function SupportPage() {
       <div className="text-muted-foreground">
         Manage users in your organisation
       </div>
+
+      {users && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.email}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
---
+++ frontend/hooks/use-users.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
+import { listUsersInOrgOrgsOrganisationIdUsersGetOptions } from '@/lib/client/@tanstack/react-query.gen'
+
+export function useUsers(organisationId: string) {
+  const query = useQuery({
+    ...listUsersInOrgOrgsOrganisationIdUsersGetOptions({
+      path: { organisation_id: organisationId },
+    }),
+  })
+
+  return {
+    ...query,
+    users: query.data ?? [],
+  }
+}
