df5a6b4f9fa737bc0c2c0baab4680d50ab5de875
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2026-06-02 10:57:16 +0100

    create hook for system admin users

--- frontend/app/user-management/page.tsx
+++ frontend/app/user-management/page.tsx
@@ -3,15 +3,19 @@
 import { Button } from '@/components/ui/button'
 import { ChevronLeft } from 'lucide-react'
 import { useRouter } from 'next/navigation'
-import { useUsers } from '@/hooks/use-users'
+import { useOrgUsers, useSystemUsers } from '@/hooks/use-users'
 import { useQuery } from '@tanstack/react-query'
 import { getUserUsersMeGetOptions } from '@/lib/client/@tanstack/react-query.gen'
 
 export default function SupportPage() {
   const router = useRouter()
+
   const { data: user } = useQuery(getUserUsersMeGetOptions())
-  const organisationId = user?.organisation_id ?? ''
-  const { users } = useUsers(organisationId)
+  const isSystemAdmin = user?.roles.includes('mhclg_support_admin')
+
+  const orgUsers = useOrgUsers(user?.organisation_id ?? '')
+  const systemUsers = useSystemUsers(isSystemAdmin)
+  const activeQuery = isSystemAdmin ? systemUsers : orgUsers
 
   return (
     <div className="mx-auto max-w-3xl pt-1">
@@ -30,53 +34,59 @@ export default function SupportPage() {
       <h1 className="text-3xl font-bold">User Management</h1>
 
       <div>
-        <p class="govuk-body pt-5">
-          Something Council <br />
-          {users && <> Total Users: {users.length}</>}
+        <p className="govuk-body pt-5">
+          {activeQuery.users && <> Total Users: {activeQuery.users.length}</>}
         </p>
       </div>
 
-      <button type="submit" class="govuk-button" data-module="govuk-button">
+      <button type="submit" className="govuk-button" data-module="govuk-button">
         Invite new user
       </button>
 
-      {users && (
-        <table class="govuk-table">
-          <thead class="govuk-table__head">
-            <tr class="govuk-table__row">
-              <th scope="col" class="govuk-table__header">
+      {activeQuery.isFetching && <p>Loading</p>}
+
+      {activeQuery.users && (
+        <table className="govuk-table">
+          <thead className="govuk-table__head">
+            <tr className="govuk-table__row">
+              <th scope="col" className="govuk-table__header">
                 Name
               </th>
-              <th scope="col" class="govuk-table__header govuk-table__header">
+              <th
+                scope="col"
+                className="govuk-table__header govuk-table__header"
+              >
                 Email
               </th>
               <th
                 scope="col"
-                class="govuk-table__header govuk-table__header--numeric"
+                className="govuk-table__header govuk-table__header--numeric"
               >
                 {/* View Account placeholder */}
               </th>
             </tr>
           </thead>
-          <tbody class="govuk-table__body">
-            {users.map((user) => (
+          <tbody className="govuk-table__body">
+            {activeQuery.users.map((user) => (
               <tr key={user.id} className="govuk-table__row">
-                <th scope="row" class="govuk-table__header">
+                <th scope="row" className="govuk-table__header">
                   {user.name}
                 </th>
-                <td class="govuk-table__cell govuk-table__cell">
+                <td className="govuk-table__cell govuk-table__cell">
                   {user.email}
                 </td>
-                <td class="govuk-table__cell govuk-table__cell--numeric">
-                  {user.roles.includes('local_authority_admin') && (
-                    <strong class="govuk-tag">Admin</strong>
-                  )}
-                  <a
-                    href="#"
-                    class="govuk-link govuk-link--no-visited-state ml-5"
-                  >
-                    View Account
-                  </a>
+                <td className="govuk-table__cell govuk-table__cell--numeric">
+                  <div className="flex justify-end gap-3">
+                    {user.roles.includes('local_authority_admin') && (
+                      <strong className="govuk-tag">Admin</strong>
+                    )}
+                    <a
+                      href="#"
+                      className="govuk-link govuk-link--no-visited-state"
+                    >
+                      View Account
+                    </a>
+                  </div>
                 </td>
               </tr>
             ))}
--- frontend/hooks/use-users.tsx
+++ frontend/hooks/use-users.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
-import { listUsersInOrgOrgsOrganisationIdUsersGetOptions } from '@/lib/client/@tanstack/react-query.gen'
+import {
+  listUsersInOrgOrgsOrganisationIdUsersGetOptions,
+  listUsersUsersGetOptions,
+} from '@/lib/client/@tanstack/react-query.gen'
 
-export function useUsers(organisationId: string) {
+export function useOrgUsers(organisationId: string) {
   const query = useQuery({
     ...listUsersInOrgOrgsOrganisationIdUsersGetOptions({
       path: { organisation_id: organisationId },
@@ -11,6 +14,17 @@ export function useUsers(organisationId: string) {
 
   return {
     ...query,
-    users: query.data ?? [],
+    users: query.data ?? undefined,
+  }
+}
+
+export function useSystemUsers(enabled: boolean | undefined) {
+  const query = useQuery({
+    ...listUsersUsersGetOptions(),
+    enabled: Boolean(enabled),
+  })
+  return {
+    ...query,
+    users: query.data ?? undefined,
   }
 }
