daf9eaab0e2eea8d36aa7f721aed524337b5d877
Rowan Hill <rowan.hill@softwire.com>
2026-06-02 10:58:34 +0100

    AIILG-482: Configure Sentry.io in all services & pipe values through Terraform

--- worker/main.py
+++ worker/main.py
@@ -1,6 +1,20 @@
 import asyncio
 
+import sentry_sdk
+
+from common.settings import get_settings
 from worker.worker_service import create_worker_service
+
+settings = get_settings()
+
+
+if settings.SENTRY_DSN:
+    sentry_init_opts = {
+        "send_default_pii": settings.ENVIRONMENT != "prod",
+        "traces_sample_rate": 1.0,
+        "profile_session_sample_rate": 0.2 if settings.ENVIRONMENT == "prod" else 1.0,
+    }
+    sentry_sdk.init(settings.SENTRY_DSN, environment=settings.ENVIRONMENT, **sentry_init_opts)
 
 if __name__ == "__main__":
     worker_service = create_worker_service()
