e212e4c68427818a027dc05c77d1864294245892
Toluwalope <toluwalopelawal1@DGTL0007.local>
2026-06-05 10:17:35 +0100

    amended signout route to delete known cookie patterns

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,7 +1,6 @@
 import logging
-import re
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, status
 from fastapi.responses import RedirectResponse
 
 from backend.utils.get_idp_logout_url import get_idp_logout_url
@@ -10,10 +9,10 @@ from common.settings import get_settings
 logger = logging.getLogger(__name__)
 settings = get_settings()
 auth_router = APIRouter(tags=["Authentication"])
- 
+
 
 ALB_AUTH_COOKIE_NAME = "X-Amzn-Oidc-Data"
-ALB_AUTH_COOKIE_SUFFIXES = ["", "-0", "-1", "-2", "-3"] # ALB can potentially set multiple cookies with these suffixes 
+ALB_AUTH_COOKIE_SUFFIXES = ["", "-0", "-1", "-2", "-3"]  # ALB can potentially set multiple cookies with these suffixes
 
 END_SESSION_ENDPOINT_STATIC = "https://sso.service.security.gov.uk/sign-out"
 
@@ -27,16 +26,15 @@ async def sign_out(request: Request) -> RedirectResponse:
 
     response = RedirectResponse(
         url=end_session_endpoint,
-        status_code=302,
+        status_code=status.HTTP_302_FOUND,
     )
 
     for suffix in ALB_AUTH_COOKIE_SUFFIXES:
         cookie_name = f"{ALB_AUTH_COOKIE_NAME}{suffix}"
-        logger.info("Clearing cookie: %s", cookie_name)
         response.delete_cookie(
             cookie_name,
             path="/",
-            domain=domain, 
+            domain=domain,
             secure=True,
             httponly=True,
         )
