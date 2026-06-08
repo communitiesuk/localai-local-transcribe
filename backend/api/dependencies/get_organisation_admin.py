695b485457b6e193427c05264b38e81834450748
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2026-06-08 11:44:58 +0100

    remove unused import

--- backend/api/dependencies/get_organisation_admin.py
+++ backend/api/dependencies/get_organisation_admin.py
@@ -2,24 +2,20 @@ from typing import Annotated
 
 from fastapi import Depends, HTTPException
 
-from backend.api.dependencies.get_session import SQLSessionDep
 from backend.api.dependencies.get_current_user import UserDep
-from backend.api.dependencies.get_organisation import OrganisationDep
+from backend.api.dependencies.get_session import SQLSessionDep
 from common.auth import is_admin_for_org
 from common.database.postgres_models import Organisation
 
 
-async def get_org_admin(
-    user: UserDep,
-    session: SQLSessionDep
-) -> Organisation:
+async def get_org_admin(user: UserDep, session: SQLSessionDep) -> Organisation:
     if not user.organisation_id:
         raise HTTPException(status_code=403, detail="User not in organisation")
-    
+
     organisation = await session.get(Organisation, user.organisation_id)
     if not organisation:
         raise HTTPException(status_code=404, detail="Organisation not found")
-    
+
     if not is_admin_for_org(user, organisation):
         raise HTTPException(
             status_code=403,
