2e4856f786e90eb6e44b4ca838aabcb79fcb6b20
imad-chowdhury-dluhc <Imad.Chowdhury1@communities.gov.uk>
2026-06-08 11:29:59 +0100

    remove magic numbers from queries

--- backend/utils/queries.py
+++ backend/utils/queries.py
@@ -1,29 +1,24 @@
-from sqlmodel import select, func, col
+from sqlmodel import col, func, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from backend.utils.constants import DEFAULT_PAGE, DEFAULT_PAGE_SIZE
 from common.database.postgres_models import Organisation, User
 
 
 async def get_users(
-        session: AsyncSession, 
-        organisation: Organisation | None = None,
-        page: int = 1,
-        page_size: int = 10,
+    session: AsyncSession,
+    organisation: Organisation | None = None,
+    page: int = DEFAULT_PAGE,
+    page_size: int = DEFAULT_PAGE_SIZE,
 ) -> list[User]:
     offset = (page - 1) * page_size
 
     statement = select(User)
 
     if organisation:
-        statement = statement.where(
-            User.organisation_id == organisation.id
-        )
+        statement = statement.where(User.organisation_id == organisation.id)
 
-    statement = (
-        statement
-        .offset(offset)
-        .limit(page_size)
-    )
+    statement = statement.offset(offset).limit(page_size)
 
     result = await session.exec(statement)
     return list(result.all())
@@ -36,9 +31,7 @@ async def get_user_count(
     statement = select(func.count(col(User.id)))
 
     if organisation:
-        statement = statement.where(
-            User.organisation_id == organisation.id
-        )
+        statement = statement.where(User.organisation_id == organisation.id)
 
     result = await session.exec(statement)
     return result.one()
