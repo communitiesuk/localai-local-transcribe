d9f6f4d2520dc3e3e04067159aac2497b6b7a6d6
Imad <150797421+imad-chowdhury-dluhc@users.noreply.github.com>
2026-06-11 12:37:20 +0100

    feat(frontend): Admin lists users (AIILG-512) (#239)
    * Update permissions in user endpoints

--- backend/utils/queries.py
+++ backend/utils/queries.py
@@ -1,9 +1,37 @@
-from sqlmodel import select
+from sqlmodel import col, func, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from backend.utils.constants import DEFAULT_PAGE, DEFAULT_PAGE_SIZE
 from common.database.postgres_models import Organisation, User
 
 
-async def get_users(session: AsyncSession, organisation: Organisation | None = None) -> list[User]:
-    statement = select(User).where(User.organisation_id == organisation.id) if organisation else select(User)
-    return list((await session.exec(statement)).all())
+async def get_users(
+    session: AsyncSession,
+    organisation: Organisation | None = None,
+    page: int = DEFAULT_PAGE,
+    page_size: int = DEFAULT_PAGE_SIZE,
+) -> list[User]:
+    offset = (page - 1) * page_size
+
+    statement = select(User)
+
+    if organisation:
+        statement = statement.where(User.organisation_id == organisation.id)
+
+    statement = statement.offset(offset).limit(page_size)
+
+    result = await session.exec(statement)
+    return list(result.all())
+
+
+async def get_user_count(
+    session: AsyncSession,
+    organisation: Organisation | None = None,
+) -> int:
+    statement = select(func.count(col(User.id)))
+
+    if organisation:
+        statement = statement.where(User.organisation_id == organisation.id)
+
+    result = await session.exec(statement)
+    return result.one()
