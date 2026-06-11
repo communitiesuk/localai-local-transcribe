fc1676496607f1779480fd2fc7cd6d529a65e048
Toluwalope <toluwalopelawal1@DGTL0007.local>
2026-06-11 17:17:12 +0100

    handles input error state where email is incorrect

--- frontend/app/invite-user/page.tsx
+++ frontend/app/invite-user/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { cn } from "@/lib/utils"
+import { set } from "react-hook-form"
 
 export default function AdminAddUserPage() {
     const router = useRouter()
-    const [inviteeName, setInviteeName] = useState("")
-    const [inviteeEmail, setInviteeEmail] = useState("")
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [hasError, setHasError] = useState(false)
 
     /*
     TODO: 
@@ -16,12 +19,18 @@ export default function AdminAddUserPage() {
 
     const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
         e.preventDefault()
+        setHasError(true)
+        return
         //TODO: Validate if email exists & is in accepted domains list
         router.push(
-            `/invite-user/confirm?name=${encodeURIComponent(inviteeName)}&email=${encodeURIComponent(inviteeEmail)}`
+            `/invite-user/confirm?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`
         )
     }
 
+    const handleCancel = (e: React.SyntheticEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        setHasError(false)
+    }
 
     return (
         <div>
@@ -40,25 +49,56 @@ export default function AdminAddUserPage() {
                         <input
                             className="govuk-input govuk-input--width-30"
                             id="invitee-name"
-                            name="inviteeName"
+                            name="name"
                             type="text"
-                            value={inviteeName}
-                            onChange={(e) => setInviteeName(e.target.value)}
+                            spellCheck="false"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                             required
                         />
                     </div>
 
-                    <div className="govuk-form-group">
-                        <label className="govuk-label" htmlFor="invitee-email-address">
+                    <div
+                        className={cn(
+                            "govuk-form-group",
+                            hasError && "govuk-form-group--error"
+                        )}
+                    >
+                        <label
+                            className="govuk-label"
+                            htmlFor="invitee-email-address"
+                        >
                             Email address
                         </label>
+
+                        {hasError && (
+                            <p
+                                id="invitee-email-address-error"
+                                className="govuk-error-message"
+                            >
+                                <span className="govuk-visually-hidden">
+                                    Error:
+                                </span>
+                                Please enter an email address with a valid domain.
+                            </p>
+                        )}
+
                         <input
-                            className="govuk-input govuk-input--width-30"
+                            className={cn(
+                                "govuk-input govuk-input--width-30",
+                                hasError && "govuk-input--error"
+                            )}
                             id="invitee-email-address"
-                            name="inviteeEmailAddress"
+                            name="emailAddress"
                             type="email"
-                            value={inviteeEmail}
-                            onChange={(e) => setInviteeEmail(e.target.value)}
+                            spellCheck="false"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            aria-describedby={
+                                hasError
+                                    ? "invitee-email-address-error"
+                                    : undefined
+                            }
                             required
                         />
                     </div>
@@ -82,6 +122,7 @@ export default function AdminAddUserPage() {
                         <a
                             href="/admin/users"
                             className="govuk-link"
+                            onClick={handleCancel}
                         >
                             Cancel
                         </a>
