a46d611dec4704dc0b7387ed9bd828ca7ef085f5
Toluwalope <toluwalopelawal1@DGTL0007.local>
2026-06-12 12:23:25 +0100

    pre-commit fixes

--- frontend/utils/allowed-domains.tsx
+++ frontend/utils/allowed-domains.tsx
@@ -2,13 +2,11 @@ export default function isAllowedDomain(
   email: string,
   allowedDomains: string[]
 ): boolean {
-  const domain = email.split("@")[1]?.toLowerCase()
+  const domain = email.split('@')[1]?.toLowerCase()
 
   if (!domain) {
     return false
   }
 
-  return allowedDomains.some(
-    (allowed) => allowed.toLowerCase() === domain
-  )
+  return allowedDomains.some((allowed) => allowed.toLowerCase() === domain)
 }
