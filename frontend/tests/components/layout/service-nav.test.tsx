9e03341741afb11b6fc3abe36f0675146cf19364
Toluwalope <toluwalope.lawal@communities.gov.uk>
2026-06-23 17:15:33 +0100

    merged with dev

--- frontend/tests/components/layout/service-nav.test.tsx
+++ frontend/tests/components/layout/service-nav.test.tsx
@@ -46,11 +46,7 @@ describe('<ServiceNav />', () => {
     const { container } = render(<ServiceNav />)
     const outer = container.querySelector('section.govuk-service-navigation')
     expect(outer).not.toBeNull()
-<<<<<<< HEAD
-    expect(outer).toHaveAttribute('data-module', 'govuk-service-navigation')
-=======
     expect(outer).not.toHaveAttribute('data-module')
->>>>>>> development
     expect(outer?.querySelector('.govuk-width-container')).not.toBeNull()
     expect(
       outer?.querySelector('.govuk-service-navigation__container')
