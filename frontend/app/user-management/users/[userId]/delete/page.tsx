98a39566a970f44fa52b905c7a408cb3b8752bf0
Imad <150797421+imad-chowdhury-dluhc@users.noreply.github.com>
2026-06-29 14:57:59 +0100

    feat(frontend): Admin delete user (AIILG-362) (#308)

--- frontend/app/user-management/users/[userId]/delete/page.tsx
+++ frontend/app/user-management/users/[userId]/delete/page.tsx
@@ -15,12 +15,32 @@ import {
   getTargetUserUsersUserIdGetOptions,
   deleteUserUsersUserIdDeleteMutation,
 } from '@/lib/client/@tanstack/react-query.gen'
+import { useBannerStore } from '@/stores/use-banner-store'
 import { Loader2 } from 'lucide-react'
+
+function formatCurrentDateTime() {
+  const now = new Date()
+
+  const time = now.toLocaleTimeString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  })
+
+  const date = now.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  })
+
+  return `${time} on ${date}`
+}
 
 export default function UserPageDelete(props: {
   params: Promise<{ userId: string }>
 }) {
   const router = useRouter()
+  const setBanner = useBannerStore((store) => store.setBanner)
 
   const { userId } = use(props.params)
 
@@ -50,6 +70,11 @@ export default function UserPageDelete(props: {
   const { mutate: deleteUser, isPending: deletePending } = useMutation({
     ...deleteUserUsersUserIdDeleteMutation(),
     onSuccess() {
+      setBanner({
+        variant: 'success',
+        title: 'Success',
+        message: `${targetUser?.name} was successfully deleted at ${formatCurrentDateTime()}`,
+      })
       router.replace(redirectPath)
     },
   })
