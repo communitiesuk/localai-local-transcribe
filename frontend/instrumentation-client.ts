e581f5bbd5ffbb115ea5de282f548007bec09745
robbfox <robbfox@outlook.com>
2026-07-13 11:38:58 +0100

    fix: update environment variable in instrmentation-client.ts

--- frontend/instrumentation-client.ts
+++ frontend/instrumentation-client.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN
-const ENVIRONMENT = process.env.ENVIRONMENT
+const ENVIRONMENT = process.env.NEXT_PUBLIC_ENVIRONMENT || 'staging'
 
 if (SENTRY_DSN) {
   Sentry.init({
