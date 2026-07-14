50512e3a803273ca8ee0f3ce9aa4ab24c5951d16
Toluwalope Lawal <toluwalope.lawal@communities.gov.uk>
2026-07-14 17:21:29 +0100

    bug(frontend): Data retention type safety (AIILG-735) (#352)
    * type safety fix with data retention option

--- frontend/lib/client/client/types.gen.ts
+++ frontend/lib/client/client/types.gen.ts
@@ -85,6 +85,7 @@ export interface ResolvedRequestOptions<
   ThrowOnError extends boolean = boolean,
   Url extends string = string,
 > extends RequestOptions<unknown, ThrowOnError, Url> {
+  headers: Headers
   serializedBody?: string
 }
 
@@ -112,7 +113,8 @@ export type RequestResult<
               : TError
           }
       ) & {
-        response: Response
+        /** response may be undefined due to a network error where no response object is produced */
+        response?: Response
       }
     >
 
@@ -131,11 +133,12 @@ type MethodFn = <
 
 type SseFn = <
   TData = unknown,
-  TError = unknown,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _TError = unknown,
   ThrowOnError extends boolean = false,
 >(
-  options: Omit<RequestOptions<TData, ThrowOnError>, 'method'>
-) => Promise<ServerSentEventsResult<TData, TError>>
+  options: Omit<RequestOptions<never, ThrowOnError>, 'method'>
+) => Promise<ServerSentEventsResult<TData>>
 
 type RequestFn = <
   TData = unknown,
@@ -177,9 +180,7 @@ export type Client = CoreClient<
  */
 export type CreateClientConfig<T extends ClientOptions = ClientOptions> = (
   override?: Config<ClientOptions & T>
-) =>
-  | Config<Required<ClientOptions> & T>
-  | Promise<Config<Required<ClientOptions> & T>>
+) => Config<Required<ClientOptions> & T>
 
 export interface TDataShape {
   body?: unknown
