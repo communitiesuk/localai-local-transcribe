df94f66deee05ead0033b606bda5a6cc5a8a3ecf
Harry Best <harry.best@softwire.com>
2026-07-14 10:53:38 +0100

    chore: rename maintenance_page_bucket to resilience_assets_bucket

--- terraform/modules/frontdoor/cloudfront.tf
+++ terraform/modules/frontdoor/cloudfront.tf
@@ -1,10 +1,10 @@
 locals {
-  origin_id             = "origin-${var.environment_name}"
-  maintenance_origin_id = "maintenance-origin-${var.environment_name}"
+  origin_id                   = "origin-${var.environment_name}"
+  resilience_assets_origin_id = "resilience-assets-origin-${var.environment_name}"
 }
 
-resource "aws_cloudfront_origin_access_identity" "maintenance_oai" {
-  comment = "OAI for maintenance page S3 bucket"
+resource "aws_cloudfront_origin_access_identity" "resilience_assets_oai" {
+  comment = "OAI for resilience assets S3 bucket"
 }
 
 #tfsec:ignore:aws-cloudfront-enable-logging: TODO we will be implementing logging later
@@ -46,11 +46,11 @@ resource "aws_cloudfront_distribution" "main" {
   }
 
   origin {
-    domain_name = module.maintenance_page_bucket.bucket_regional_domain_name
-    origin_id   = local.maintenance_origin_id
+    domain_name = module.resilience_assets_bucket.bucket_regional_domain_name
+    origin_id   = local.resilience_assets_origin_id
 
     s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.maintenance_oai.cloudfront_access_identity_path
+      origin_access_identity = aws_cloudfront_origin_access_identity.resilience_assets_oai.cloudfront_access_identity_path
     }
   }
 
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "main" {
     cached_methods         = ["GET", "HEAD"]
     cache_policy_id        = aws_cloudfront_cache_policy.main.id
     path_pattern           = "/govuk-frontend-5.11.2.min.css"
-    target_origin_id       = local.maintenance_origin_id
+    target_origin_id       = local.resilience_assets_origin_id
     viewer_protocol_policy = "redirect-to-https"
   }
 
@@ -69,7 +69,7 @@ resource "aws_cloudfront_distribution" "main" {
     cached_methods         = ["GET", "HEAD"]
     cache_policy_id        = aws_cloudfront_cache_policy.main.id
     path_pattern           = "/.well-known/security.txt"
-    target_origin_id       = local.maintenance_origin_id
+    target_origin_id       = local.resilience_assets_origin_id
     viewer_protocol_policy = "redirect-to-https"
   }
 
@@ -78,7 +78,7 @@ resource "aws_cloudfront_distribution" "main" {
     cached_methods         = ["GET", "HEAD"]
     cache_policy_id        = aws_cloudfront_cache_policy.main.id
     path_pattern           = var.maintenance_mode_on ? "*" : "/maintenance"
-    target_origin_id       = local.maintenance_origin_id
+    target_origin_id       = local.resilience_assets_origin_id
     viewer_protocol_policy = "redirect-to-https"
   }
 
