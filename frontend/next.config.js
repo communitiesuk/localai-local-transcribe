72dd3199411458f9eaa35e05214aee9e965af5b3
robbfox <robbfox@outlook.com>
2026-07-15 11:23:30 +0100

    refactor: simplify next.config.js by removing unnecessary config wrapping and updating Sentry settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,23 +1,9 @@
-import { withSentryConfig } from '@sentry/nextjs'
-
-let nextConfig = {
-  output: 'standalone',
-  sassOptions: {
-    includePaths: ['./node_modules'],
-    quietDeps: true,
-  },
-}
-
 const sentryConfig = {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
   webpack: {
     treeShaking: {
       removeDebugLogging: true,
-    },
-    automaticVercelMonitors: true,
-    reactComponentAnnotation: {
-      enabled: true,
     },
   },
   org: 'communitiesuk',
@@ -38,24 +24,19 @@ const sentryConfig = {
   },
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: '/monitoring',
 
   // Hides source maps from generated client bundles
   hideSourceMaps: true,
 
+  // Physically deletes source maps from build output after upload completes
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
+
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobshttps://docs.sentry.io/product/sentry-basics/integrate-backend/configuration-options/
+  // Enables automatic instrumentation of Vercel Cron Monitors. 
   automaticVercelMonitors: true,
 }
-
-nextConfig = withSentryConfig(nextConfig, sentryConfig)
-
-export default nextConfig
