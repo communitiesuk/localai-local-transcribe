9ade7b09877224e82952fdc97e986f01808e8fc2
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2026-08-11 11:42:32 +0100

    Restyle document editor to match form editor

--- frontend/app/templates/components/document-template-editor.tsx
+++ frontend/app/templates/components/document-template-editor.tsx
@@ -2,6 +2,7 @@
 
 import { TemplateEditorToolbar } from '@/app/templates/components/editor/editor-toolbar'
 import {
+  GovukDetails,
   GovukErrorSummary,
   GovukFormGroup,
   GovukHint,
@@ -54,7 +55,6 @@ export const DocumentTemplateEditor = ({
         <GovukErrorSummary
           title="There is a problem"
           errorList={errorList}
-          className="govuk-!-margin-bottom-6"
           data-testid="error-summary"
         />
       )}
@@ -64,16 +64,11 @@ export const DocumentTemplateEditor = ({
           Template details
         </h2>
         <GovukHint className="govuk-!-margin-bottom-4">
-          Add a name and description so you can find your template later. Name
-          and description are not used to generate your minute — add structure
-          and style instructions to the template content field below.
+          Add a title and description to help you find your template later
         </GovukHint>
 
-        <GovukFormGroup
-          hasError={!!errors.name}
-          className="govuk-!-margin-bottom-4"
-        >
-          <GovukLabel htmlFor="template-name">Template name</GovukLabel>
+        <GovukFormGroup hasError={!!errors.name}>
+          <GovukLabel htmlFor="template-name">Title</GovukLabel>
           {errors.name?.message && (
             <p id="template-name-error" className="govuk-error-message">
               <span className="govuk-visually-hidden">Error:</span>{' '}
@@ -82,11 +77,10 @@ export const DocumentTemplateEditor = ({
           )}
           <GovukInput
             id="template-name"
-            className="govuk-!-margin-top-1"
             aria-invalid={!!errors.name}
             aria-describedby={errors.name ? 'template-name-error' : undefined}
             {...form.register('name', {
-              required: { value: true, message: 'Enter a template name' },
+              required: { value: true, message: 'Enter a title' },
             })}
           />
         </GovukFormGroup>
@@ -104,7 +98,6 @@ export const DocumentTemplateEditor = ({
           )}
           <GovukInput
             id="template-description"
-            className="govuk-!-margin-top-1"
             aria-invalid={!!errors.description}
             aria-describedby={
               errors.description ? 'template-description-error' : undefined
@@ -116,22 +109,92 @@ export const DocumentTemplateEditor = ({
         </GovukFormGroup>
       </div>
 
+      <hr className="govuk-section-break govuk-section-break--visible govuk-!-margin-bottom-0" />
+
       <div>
+        <h2 className="govuk-heading-m govuk-!-margin-bottom-1">
+          Content and style
+        </h2>
+
+        <GovukDetails summary="Useful tips" className="govuk-!-margin-bottom-4">
+          <p className="govuk-body">
+            Customise your template based on how you want your document to be
+            presented.
+          </p>
+          <p className="govuk-body govuk-!-font-weight-bold govuk-!-margin-bottom-1">
+            Headings:
+          </p>
+          <ul className="govuk-list govuk-list--bullet">
+            <li>
+              write each heading how you want it to appear in the document
+            </li>
+            <li>headings are case sensitive</li>
+          </ul>
+          <p className="govuk-body govuk-!-font-weight-bold govuk-!-margin-bottom-1">
+            Placeholder text:
+          </p>
+          <ul className="govuk-list govuk-list--bullet">
+            <li>
+              state the information you&rsquo;d like to include, or questions
+              you&rsquo;d like answered, in each section
+            </li>
+            <li>
+              the information will be pulled from the transcript into the
+              document
+            </li>
+          </ul>
+          <p className="govuk-body govuk-!-font-weight-bold govuk-!-margin-bottom-1">
+            Format instructions:
+          </p>
+          <ul className="govuk-list govuk-list--bullet govuk-!-margin-bottom-0">
+            <li>
+              state how you&rsquo;d like the information to be presented, for
+              example:
+              <ul className="govuk-list govuk-list--bullet">
+                <li>tone (more or less formal)</li>
+                <li>specific language to include</li>
+                <li>length</li>
+                <li>bulleted list</li>
+                <li>any text to make bold</li>
+              </ul>
+            </li>
+          </ul>
+        </GovukDetails>
+
+        <GovukFormGroup className="govuk-!-margin-bottom-4">
+          <GovukLabel htmlFor="template-heading">
+            Template heading (optional)
+          </GovukLabel>
+          <GovukHint
+            id="template-heading-hint"
+            className="govuk-!-margin-bottom-1"
+          >
+            This will appear at the top of your document above Section 1 (it can
+            be different from the template title, which will not appear in the
+            document)
+          </GovukHint>
+          <GovukInput
+            id="template-heading"
+            aria-describedby="template-heading-hint"
+            {...form.register('heading')}
+          />
+        </GovukFormGroup>
+
         <GovukFormGroup
           hasError={!!errors.content}
           className="govuk-!-margin-bottom-0"
         >
-          <GovukLabel htmlFor="template-content" size="m">
+          <h3 className="govuk-heading-s govuk-!-margin-bottom-2">
             Template content
-          </GovukLabel>
+          </h3>
           <GovukHint
             id="template-content-hint"
-            className="govuk-!-margin-top-1 govuk-!-margin-bottom-3"
+            className="govuk-!-margin-bottom-2"
           >
-            The template content should look how you would like the minutes to
-            look. Use placeholder text to describe what you would like in each
-            section and provide style guidance, including examples if necessary.
-            You may need to iterate on your template to get the best results.
+            The template content should look how you would like the output to
+            look. Use placeholder text combined with format instructions to
+            describe what you would like in each section and how you want the
+            information presented.
           </GovukHint>
           {errors.content?.message && (
             <p
@@ -192,6 +255,7 @@ const ControlledEditor = ({
       id="template-content"
       tabIndex={-1}
       onFocus={() => editor?.commands.focus()}
+      aria-describedby="template-content-hint"
       className={cn('govuk-textarea p-0', hasError && 'govuk-textarea--error')}
     >
       <TemplateEditorToolbar editor={editor} />
