eedf922ea85edfe4fe0b74bf8cf416c62aa83ed9
robbfox <robbfox@outlook.com>
2026-08-25 15:59:20 +0100

    refactor: simplify RecordingState type definition format

--- frontend/stores/use-recording-ui-store.ts
+++ frontend/stores/use-recording-ui-store.ts
@@ -1,11 +1,7 @@
 import { create } from 'zustand'
 
 export type RecordingState =
-  | 'idle'
-  | 'starting'
-  | 'recording'
-  | 'paused'
-  | 'stopped'
+  'idle' | 'starting' | 'recording' | 'paused' | 'stopped'
 
 export type RecordingUiStore = {
   recordingState: RecordingState
