3a140e3e5b6676e83fddc1013b545fe013b8e041
robbfox <robbfox@outlook.com>
2026-09-15 15:14:42 +0100

    fix(terraform): update noncurrent version expiration days to use cloudwatch log expiration

--- terraform/modules/monitoring/s3.tf
+++ terraform/modules/monitoring/s3.tf
@@ -4,7 +4,7 @@ module "s3_bucket" {
   access_log_bucket_name                 = "local-transcribe-cloudtrail-${var.environment_name}-access-logs"
   bucket_name                            = "local-transcribe-cloudtrail-${var.environment_name}"
   access_s3_log_expiration_days          = var.access_s3_log_expiration_days
-  noncurrent_version_expiration_days     = var.access_s3_log_expiration_days
+  noncurrent_version_expiration_days     = var.cloudwatch_log_expiration_days
   expiration_days                        = var.cloudwatch_log_expiration_days
   abort_incomplete_multipart_upload_days = var.abort_incomplete_multipart_upload_days
   policy                                 = data.aws_iam_policy_document.bucket_policy.json
