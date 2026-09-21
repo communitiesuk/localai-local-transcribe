84fc02cec5f714df069f24017d4e0d8e2c36d588
Patryk.Kuchta <patryk.kuchta@softwire.com>
2026-09-21 15:47:14 +0100

    feat: new flow implementation

--- common/services/minute_handler_service.py
+++ common/services/minute_handler_service.py
@@ -72,6 +72,7 @@ class MinuteHandlerService:
                         explanation=detail.explanation,
                     )
                     for detail in score.categories
+                    if not passed
                 ],
             )
             session.add(guardrail_result)
