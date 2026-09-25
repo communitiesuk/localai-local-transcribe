134a5065d48f0aa07302c16b1952d7e235a81ca2
Imad <150797421+imad-chowdhury-dluhc@users.noreply.github.com>
2026-09-25 13:26:04 +0000

    feat: update banner text and add email address (#537)
    * feat: update banner text and add email address
    
    * test: update phase banner wording
    
    * test: update test to check phase banner link

--- frontend/components/layout/phase-banner.tsx
+++ frontend/components/layout/phase-banner.tsx
@@ -1,11 +1,20 @@
+import { GovukTag } from '@/components/govuk'
+
 export function PhaseBanner() {
   return (
     <div className="govuk-phase-banner">
       <p className="govuk-phase-banner__content">
-        <strong className="govuk-tag govuk-phase-banner__content__tag">
-          Alpha
-        </strong>
-        <span className="govuk-phase-banner__text">This is a new service.</span>
+        <GovukTag className="govuk-phase-banner__content__tag">Beta</GovukTag>
+        <span className="govuk-phase-banner__text">
+          This is a new service –{' '}
+          <a
+            className="govuk-link"
+            href="mailto:LocalTranscribe@communities.gov.uk"
+          >
+            email us your feedback
+          </a>{' '}
+          to help us improve it.
+        </span>
       </p>
     </div>
   )
